54d179b79dd4934d50c3d3c48699f692f4b467e1
liqicong <952267787@qq.com>
2019-09-16 10:02:48 +0800

    fisrt commit

---
+++ src/main/java/com/lqc/sword/SwordApplication.java
@@ -0,0 +1,13 @@
+package com.lqc.sword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwordApplication.class, args);
+    }
+
+}
